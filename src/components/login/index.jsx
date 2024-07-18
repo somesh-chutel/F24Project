e7b0ca463b89c8d30980cc1a738a837318cee547
srocknik <somesh.chutel90@gmail.com>
2024-07-18 08:29:33 +0530

    second commit

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,9 +1,70 @@
-import './index.css'
+import { useState } from "react";
+import "./index.css";
 
-const Login = ()=> <h1> Login Component </h1>
+const Login = () => {
+  const [allValues, setValues] = useState({
+    username: "",
+    password: "",
+    showErrorMsg: false,
+    errorMsg: "",
+  });
 
+  const onSubmitUserDetails = async (e) => {
+    e.preventDefault();
 
+    const api = "https://apis.ccbp.in/login";
 
+    const userDetails = {
+      username: allValues.username,
+      password: allValues.password,
+    };
 
+    const options = {
+      method: "Post",
+      body: JSON.stringify(userDetails),
+    };
+
+    const response = await fetch(api, options);
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  return (
+    <div className="login-cont">
+      <form className="my-form" onSubmit={onSubmitUserDetails}>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={(e) => {
+              setValues({ ...allValues, username: e.target.value });
+            }}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your username with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={(e)=>{ setValues({...allValues,password : e.target.value})}}
+          />
+        </div>
+        <br />
+        <button type="submit" className="btn btn-primary form-control">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
